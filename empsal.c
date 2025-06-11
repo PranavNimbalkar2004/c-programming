23402d750426d6a52bde9e5e1d95f9d930ea109e
PranavNimbalkar2004 <pranavnimbalkar02@gmail.com>
2025-06-11 13:40:27 +0530

    Max.c

--- empsal.c
+++ empsal.c
@@ -1,17 +1,34 @@
 #include<stdio.h>
 int main()
 {
-    float  salary,DA,hra,tax,bs;
-    printf("enter salary");
-    scanf("%d",&salary);
-     printf("enter DA");
-    scanf("%d",&DA); 
-    printf("enter tax");
-    scanf("%d",&tax);
-     printf("enter bs");
-    scanf("%d",&bs);
-    salary=bs+((bs*(hra+DA-tax))/100);
-    printf("salary = %.2f",salary);
+    float bs, totalsalary, da, hra, tax;
+    int eid;
+    char ename[100];
+    FILE *fp;
+    fp = fopen("salarySlip.txt","w");
+    printf("Enter Employee Details :");
+    printf("\nID :");
+    scanf("%d",&eid);
+    printf("Name :");
+    scanf("\n%d",&ename);
+    printf("Basic Salary :");
+    scanf("\n%d",&bs);
+    printf("DA (%%) :");
+    scanf("\n%f",&da);
+    printf("HRA (%%) :");
+    scanf("\n%f",&hra);
+    printf("TAX (%%) :");
+    scanf("\n%f",&tax);
+    totalsalary = bs + ((bs * (da+hra-tax))/100);
+    fprintf(fp,"\n----------------------Employee Salary Slip----------------------");
+    fprintf(fp,"\n\t\tID              : %d",eid);
+    fprintf(fp,"\n\t\tNmae             : %s",ename);
+    fprintf(fp,"\n\t\tBasic Salary              : %d",bs);
+    fprintf(fp,"\n\t\tDA(%%)            : %.2f",da);
+    fprintf(fp,"\n\t\tHRA(%%)           : %.2f",hra);
+    fprintf(fp,"\n\t\tTAX(%%)          : %.2f",tax);
+    fprintf(fp,"\n\t\tTotal Salary          : %.2f",totalsalary);
+   
     return 0;
     
 }
---
+++ positivenegaivet.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter a Number");
+    scanf("%d",&n);
+    (n>0)?printf("no is positive"): printf("no is negative");
+return 0;
+}
