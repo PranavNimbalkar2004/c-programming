d0c306112db3169319fc5466f115df3215a3cc55
PranavNimbalkar2004 <pranavnimbalkar02@gmail.com>
2025-06-13 18:47:46 +0530

    A2positivenegativeQ2

---
+++ A2positivenegativeQ2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a No:");
+    scanf("%d",&n);
+    if(n>0)
+    printf("No is positive");
+else 
+
+    printf("No is negative");
+return 0;
+
+}
