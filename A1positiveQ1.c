e0443e33f7fd9c3f2df8fe9b6bb30f6a2f3c000b
PranavNimbalkar2004 <pranavnimbalkar02@gmail.com>
2025-06-13 18:38:20 +0530

    A1positiveQ1

---
+++ A1positiveQ1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter a Number");
+    scanf("%d",&n);
+    if(n>0)
+    printf("no is positive");
+return 0;
+}
---
+++ employe.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int eid;
+    char name[100];
+    float bs,da,hra,tax,totalsalary,inhand;
+    printf("enter employe details\n");
+    printf("eid:");
+    scanf("%d",%eid);
+    printf("name:");
+    scanf("%s",&name);
+    printf("bs:");
+    scanf("")
+
+
+    if(bs>=200000)
+    tax=20;
+else if(bs>=150000)
+tax=15;
+else if(bs>=100000)
+tax=10;
+else if(bs>=80000)
+tax=5;
+else if(bs>=50000)
+tax=3;
+else
+tax=0;
+totalsalary=bs+((bs*(da+hra-tax)/100));
+inhand=totalsalary;
+
+printf("\n eid:%d",id);
+printf("\n name:%s",name);
+printf("\n basic salary:%f",bs);
+printf("\n da(%%)"da);
+printf("\n");
+printf("\n")
+
+}
