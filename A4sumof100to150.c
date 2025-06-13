1fe50b109a28675c96e06c05484c95985d02d75d
PranavNimbalkar2004 <pranavnimbalkar02@gmail.com>
2025-06-13 20:10:02 +0530

    sumof100to150

---
+++ A4sumof100to150.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int  i,sum=0;
+    for(i=100;i<=150;i++)
+    {
+        sum +=i;
+    
+    }
+    printf("sum of no from 100to150:%d\n",sum);
+    return 0;
+    
+}
