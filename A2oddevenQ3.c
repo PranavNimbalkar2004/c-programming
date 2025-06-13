c99c97ad3d7b7c079c1fadba18b5a8603c260268
PranavNimbalkar2004 <pranavnimbalkar02@gmail.com>
2025-06-13 18:59:35 +0530

    A2oddevenQ3.c

---
+++ A2oddevenQ3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+   int a;
+   printf("Enter any Number:");
+   scanf("%d",&a);
+   if(a%2==0)
+   printf("No is even");
+else
+   printf("No is odd");
+return 0;
+
+}
